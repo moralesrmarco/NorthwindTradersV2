846def7a59c276aa0043684f4185d69738274b0b
moralesrmarco <marco2003_6@hotmail.com>
2025-07-31 12:24:49 -0600

    Se terminó de modificar RptGraficaVentasAnuales

--- NorthwindTraders/FrmRptGraficaVentasAnuales.cs
+++ NorthwindTraders/FrmRptGraficaVentasAnuales.cs
@@ -79,37 +79,37 @@ namespace NorthwindTraders
             string filtroAños = string.Join(",", años);
 
             string query = $@"
-                                    WITH Meses AS (
-                                    SELECT 1 AS Mes, 'Ene' AS NombreMes
-                                    UNION ALL SELECT 2, 'Feb'
-                                    UNION ALL SELECT 3, 'Mar'
-                                    UNION ALL SELECT 4, 'Abr'
-                                    UNION ALL SELECT 5, 'May'
-                                    UNION ALL SELECT 6, 'Jun'
-                                    UNION ALL SELECT 7, 'Jul'
-                                    UNION ALL SELECT 8, 'Ago'
-                                    UNION ALL SELECT 9, 'Sep'
-                                    UNION ALL SELECT 10, 'Oct'
-                                    UNION ALL SELECT 11, 'Nov'
-                                    UNION ALL SELECT 12, 'Dic'
-                                    ),                            
-                                    Ventas AS (
-                                    SELECT 
-                                        YEAR(o.OrderDate) AS Año,
-                                        MONTH(o.OrderDate) AS Mes,
-                                        SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)) AS Total
-                                    FROM Orders AS o
-                                    INNER JOIN [Order Details] AS od
-                                        ON o.OrderID = od.OrderID
-                                    WHERE YEAR(o.OrderDate) IN ({filtroAños})
-                                    GROUP BY YEAR(o.OrderDate), MONTH(o.OrderDate)
-                                    )
-                                    SELECT m.Mes, m.NombreMes, y.Año, ISNULL(v.Total, 0) AS Total
-                                    FROM Meses AS m
-                                    CROSS JOIN (SELECT DISTINCT Año FROM Ventas) As y
-                                    LEFT JOIN Ventas As v ON m.Mes = v.Mes AND y.Año = v.Año
-                                    ORDER BY m.Mes, y.Año;
-                                    ";
+                            WITH Meses AS (
+                            SELECT 1 AS Mes, 'Ene' AS NombreMes
+                            UNION ALL SELECT 2, 'Feb'
+                            UNION ALL SELECT 3, 'Mar'
+                            UNION ALL SELECT 4, 'Abr'
+                            UNION ALL SELECT 5, 'May'
+                            UNION ALL SELECT 6, 'Jun'
+                            UNION ALL SELECT 7, 'Jul'
+                            UNION ALL SELECT 8, 'Ago'
+                            UNION ALL SELECT 9, 'Sep'
+                            UNION ALL SELECT 10, 'Oct'
+                            UNION ALL SELECT 11, 'Nov'
+                            UNION ALL SELECT 12, 'Dic'
+                            ),                            
+                            Ventas AS (
+                            SELECT 
+                                YEAR(o.OrderDate) AS Año,
+                                MONTH(o.OrderDate) AS Mes,
+                                SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)) AS Total
+                            FROM Orders AS o
+                            INNER JOIN [Order Details] AS od
+                                ON o.OrderID = od.OrderID
+                            WHERE YEAR(o.OrderDate) IN ({filtroAños})
+                            GROUP BY YEAR(o.OrderDate), MONTH(o.OrderDate)
+                            )
+                            SELECT m.Mes, m.NombreMes, y.Año, ISNULL(v.Total, 0) AS Total
+                            FROM Meses AS m
+                            CROSS JOIN (SELECT DISTINCT Año FROM Ventas) As y
+                            LEFT JOIN Ventas As v ON m.Mes = v.Mes AND y.Año = v.Año
+                            ORDER BY m.Mes, y.Año;
+                            ";
             try
             {
                 using (var cn = new SqlConnection(NorthwindTraders.Properties.Settings.Default.NwCn))
